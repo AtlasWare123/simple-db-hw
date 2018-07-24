237e96c51a1f6164e3457f26c101c482e8516c94
Unlucky <unlucky1990@gmail.com>
2018-07-24 13:58:10 +0800

    implement Insert

--- src/java/simpledb/Insert.java
+++ src/java/simpledb/Insert.java
@@ -1,5 +1,7 @@
 package simpledb;
 
+import java.io.IOException;
+
 /**
  * Inserts tuples read from the child operator into the tableId specified in the
  * constructor
@@ -7,6 +9,13 @@ package simpledb;
 public class Insert extends Operator {
 
     private static final long serialVersionUID = 1L;
+
+    final private TupleDesc td = new TupleDesc(new Type[]{Type.INT_TYPE});
+
+    private TransactionId tid;
+    private OpIterator child;
+    private int tableId;
+    private boolean called;
 
     /**
      * Constructor.
@@ -19,23 +28,39 @@ public class Insert extends Operator {
      */
     public Insert(TransactionId t, OpIterator child, int tableId) throws DbException {
         // some code goes here
+        this.tid = t;
+        this.child = child;
+        this.tableId = tableId;
+        this.called = false;
     }
 
     public TupleDesc getTupleDesc() {
         // some code goes here
-        return null;
+        return this.td;
     }
 
     public void open() throws DbException, TransactionAbortedException {
         // some code goes here
+        if (this.child == null) {
+            throw new DbException("OpIterator is null, can't be opened");
+        }
+        this.child.open();
     }
 
     public void close() {
         // some code goes here
+        if (this.child != null) {
+            this.child.close();
+        }
     }
 
     public void rewind() throws DbException, TransactionAbortedException {
         // some code goes here
+        if (this.child == null) {
+            throw new DbException("OpIterator is null, can't rewind it");
+        }
+        this.child.rewind();
+        this.called = false;
     }
 
     /**
@@ -53,17 +78,36 @@ public class Insert extends Operator {
      */
     protected Tuple fetchNext() throws TransactionAbortedException, DbException {
         // some code goes here
-        return null;
+        if (this.called) {
+            return null;
+        }
+        this.called = true;
+        DbFile dbFile = Database.getCatalog().getDatabaseFile(this.tableId);
+        int cnt = 0;
+        while (child.hasNext()) {
+            try {
+                dbFile.insertTuple(this.tid, this.child.next());
+                cnt++;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        Tuple result = new Tuple(this.td);
+        result.setField(0, new IntField(cnt));
+        return result;
     }
 
     @Override
     public OpIterator[] getChildren() {
         // some code goes here
-        return null;
+        return new OpIterator[] { this.child };
     }
 
     @Override
     public void setChildren(OpIterator[] children) {
         // some code goes here
+        if (children != null && children.length > 0) {
+            this.child = children[0];
+        }
     }
 }
